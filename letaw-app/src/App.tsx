cd16aa85c3275717c8c13f92bc8a8df4645b75da
David <david-mb14@DL-MBP14.local>
2023-03-20 20:23:10 -0700

    removes unused code and adds dependencies

--- letaw-app/src/App.tsx
+++ letaw-app/src/App.tsx
@@ -1,24 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { About } from './pages/About';
+import { Resume } from './pages/Resume';
 
 function App() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const handleMenuClick = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div className="App">
+        <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-left">
+          <button
+            className="navbar-toggler border-0"
+            type="button"
+            onClick={handleMenuClick}
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className={`navbar-collapse bg-dark offcanvas-collapse ${showMenu ? 'open' : ''}`}
+            id="navbarsExampleDefault"
+          >
+            <ul className="navbar-nav flex-grow-1">
+              <li className="nav-item">
+                <Link to="/" className="nav-link" onClick={handleMenuClick}>
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/about" className="nav-link" onClick={handleMenuClick}>
+                  About
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/resume" className="nav-link" onClick={handleMenuClick}>
+                  Resume
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <div className="container-fluid">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/resume" element={<Resume />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>Welcome to My App</h1>
+      <p>This is the home page.</p>
     </div>
   );
 }
